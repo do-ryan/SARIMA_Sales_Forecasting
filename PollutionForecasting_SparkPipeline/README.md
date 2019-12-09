491ff495fc72752f5339fa287c18b2adeacf7baa
Ryan Do <ryan.jg.do@gmail.com>
2019-12-09 00:05:11 -0500

    complete

---
+++ PollutionForecasting_SparkPipeline/README.md
@@ -0,0 +1,45 @@
+Feature Engineering Approach:
+-----
+<b>1. Using atmospheric pollutant features to predict the AQI target label</b>
+
+The Beijing Air Quality Index Dataset was used to draw target label data (AQI vs. time) for the regression.
+A simple and possibly sufficiently effective approach is to simply feed this uni-variate
+data into fitting time-series model such as RNN or SARIMA but engineering more finely descriptive
+features would be more effective.
+
+I leveraged the Beijing Multi-Site Air-Quality Data Set, which features a variety of 
+air quality metrics for 12 different sites including various gas concentrations. 
+I made use of 12 of these metrics for all 12 sites which increases the dimensionality of
+my dataset by 144 therefore giving more information to the model downstream. Since the labels and 
+features are from different sources (though they are both hourly),
+I joined hour to hour between the two sets and only keep the intersection.
+
+Some data cleaning was involved where I used regular expressions to deal with missing data and
+extract the date fields from strings. 
+
+<b>2. Aggregate all districts by collect_list for every given hour</b> 
+
+For the multi-site air quality set, since there are possibly multiple records for every 
+time point (12 sites), I needed to determine a way to aggregate the records. Taking an average would 
+mean I lose a lot information on the distribution of the air quality metrics between
+sites therefore that would not be a great approach. A better approach that I took was to aggregate
+the features for each time point into a list containing the features for each monitoring station.
+Actually, these lists need to be padded to the longest one but I had a busy weekend so I didn't get 
+to that.
+
+All of the air quality measures seemed to have a fairly significant variance and were all relevant as AQI measures
+therefore I kept all of them as features in my dataset. However, the station names were omitted since this category
+is implicit in the dimensionality of each of the other features. 
+Wind direction was therefore the only categorical feature, which I one-hot encoded. 
+
+All 12 of the features are vectorized into the features column, which is to be fed as input into the eventual model.
+The label is the aqi column.
+
+
+How to Run:
+-----
+
+Submit `main.py` to spark-submit or run `python3 main.py` given that Spark, PySpark 2.4.3 and Python 3.7.4 are installed. 
+
+```SQLContext(SparkContext()).sql("SELECT * FROM parquet.`output/beijing_features_aqi_target.parquet`")```
+will query a dataframe of the saved parquet file.
--- PollutionForecasting_SparkPipeline/main.py
+++ PollutionForecasting_SparkPipeline/main.py
@@ -1,11 +1,15 @@
-from pyspark.sql import SQLContext, Row
-from pyspark.sql.types import *
 from pyspark import SparkFiles, SparkContext
-import pyspark
+from pyspark.sql import SQLContext, Row, Column
+from pyspark.sql.types import *
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
+from pyspark.sql.functions import udf
+
 from zipfile import ZipFile
 from urllib.request import urlopen
 from io import BytesIO
 import random
+import re
 random.seed(1)
 
 
@@ -41,41 +45,102 @@ def create_feature_df(zip_url):
                          StructField('WSPM', FloatType(), True),
                          StructField('station', StringType(), True),
                          ])
-    row_rdd = rdd.map(lambda x: Row(No=int(x[0]),
-                                    year=int(x[1]),
-                                    month=int(x[2]),
-                                    day=int(x[3]),
-                                    hour=int(x[4]),
-                                    PM2_5=float(x[5]),
-                                    PM10=float(x[6]),
-                                    SO2=float(x[7]),
-                                    NO2=float(x[8]),
-                                    CO=float(x[9]),
-                                    O3=float(x[10]),
-                                    TEMP=float(x[11]),
-                                    PRES=float(x[12]),
-                                    DEWP=float(x[13]),
-                                    RAIN=float(x[14]),
+    # address missing data with regex, cast to appropriate type
+    row_rdd = rdd.map(lambda x: Row(No=int(re.sub(r"[a-z|A-Z']+", "0", x[0])),
+                                    year=int(re.sub(r"[a-z|A-Z']+", "0", x[1])),
+                                    month=int(re.sub(r"[a-z|A-Z']+", "0", x[2])),
+                                    day=int(re.sub(r"[a-z|A-Z']+", "0", x[3])),
+                                    hour=int(re.sub(r"[a-z|A-Z']+", "0", x[4])),
+                                    PM2_5=float(re.sub(r"[a-z|A-Z']+", "0", x[5])),
+                                    PM10=float(re.sub(r"[a-z|A-Z']+", "0", x[6])),
+                                    SO2=float(re.sub(r"[a-z|A-Z']+", "0", x[7])),
+                                    NO2=float(re.sub(r"[a-z|A-Z']+", "0", x[8])),
+                                    CO=float(re.sub(r"[a-z|A-Z']+", "0", x[9])),
+                                    O3=float(re.sub(r"[a-z|A-Z']+", "0", x[10])),
+                                    TEMP=float(re.sub(r"NA|TEMP", "0", x[11])),
+                                    PRES=float(re.sub(r"[a-z|A-Z']+", "0", x[12])),
+                                    DEWP=float(re.sub(r"[a-z|A-Z']+", "0", x[13])),
+                                    RAIN=float(re.sub(r"[a-z|A-Z']+", "0", x[14])),
                                     wd=x[15],
-                                    WSPM=float(x[16]),
+                                    WSPM=float(re.sub(r"[a-z|A-Z']+", "0", x[16])),
                                     station=x[17]))
-    breakpoint()
-    return sqlContext.createDataFrame(row_rdd)  # consider all neighbourhoods as equal weight
+    return sqlContext.createDataFrame(row_rdd, schema=schema)  # consider all neighbourhoods as equal weight
 
 
 def create_target_df(csv_url):
     sc.addFile(csv_url)
-    schema = StructType([StructField('reading', IntegerType(), True),
-                         StructField('date', TimestampType(), True),
+    schema = StructType([StructField('reading', IntegerType(), True),  # cast to appropriate type
+                         StructField('date', StringType(), True),
                          StructField('aqi', IntegerType(), True)])
-    return sqlContext.read.csv(path=SparkFiles.get("beijing-aqm.txt"),
-                               sep='\t',
-                               schema=schema,
-                               enforceSchema=True,
-                               header=True)
+    df = sqlContext.read.csv(path=SparkFiles.get("beijing-aqm.txt"),
+                             sep='\t',
+                             header=True,
+                             schema=schema)
+    # extract date from string using regex
+    df = df.withColumn('month_t', udf(lambda x: re.findall(r"[\d']+", x)[0], StringType())('date'))
+    df = df.withColumn('day_t', udf(lambda x: re.findall(r"[\d']+", x)[1], StringType())('date'))
+    df = df.withColumn('year_t', udf(lambda x: '20' + re.findall(r"[\d']+", x)[2], StringType())('date'))
+    df = df.withColumn('hour_t', udf(lambda x: re.findall(r"[\d']+", x)[3], StringType())('date'))
+    df = df.withColumn('minute_t', udf(lambda x: re.findall(r"[\d']+", x)[4], StringType())('date'))
+    return df
 
 
 feature_df = create_feature_df(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/00501/PRSA2017_Data_20130301-20170228.zip')
 target_df = create_target_df(
-    'https://datahub.ckan.io/dataset/610fb217-0499-40e7-b53f-a50c9b02b98f/resource/772b62d8-0847-4104-ad97-ceac7fb0438d/download/beijing-aqm.txt')
+    'https://datahub.ckan.io/dataset/610fb217-0499-40e7-b53f-a50c9b02b98f/'
+    'resource/772b62d8-0847-4104-ad97-ceac7fb0438d/download/beijing-aqm.txt')
+# The data from the two sources need to be inner joined to get the intersection of hourly time points.
+inner_join = feature_df.join(other=target_df, on=[feature_df.year == target_df.year_t,
+                                                  feature_df.month == target_df.month_t,
+                                                  feature_df.day == target_df.day_t,
+                                                  feature_df.hour == target_df.hour_t], how='inner')
+
+# One hot encode wind direction (only categorical feature)
+stringIndexer = StringIndexer(inputCol="wd", outputCol="wd_encoded")
+encoder = OneHotEncoder(dropLast=False, inputCol="wd_encoded", outputCol="WD_VEC")
+assembler = VectorAssembler(
+    inputCols=['PM2_5', 'PM10', 'SO2', 'NO2', 'CO', 'O3', 'TEMP', 'PRES', 'DEWP', 'RAIN', 'WSPM', 'WD_VEC'],
+    outputCol="features")
+
+# Assemble a pipeline
+pipeline = Pipeline(stages=[stringIndexer, encoder, assembler])
+pipelineModel = pipeline.fit(inner_join)
+unagg_df = pipelineModel.transform(inner_join)
+
+# Aggregate all districts. Aggregate as a list in order to retain information on each district.
+# AQI can be aggregated in any way since there is only one AQI record per hour.
+dataset = unagg_df.groupby('year_t', 'month_t', 'day_t', 'hour_t').agg(
+    {'PM2_5': 'collect_list',
+     'PM10': 'collect_list',
+     'SO2': 'collect_list',
+     'NO2': 'collect_list',
+     'CO': 'collect_list',
+     'O3': 'collect_list',
+     'TEMP': 'collect_list',
+     'PRES': 'collect_list',
+     'DEWP': 'collect_list',
+     'RAIN': 'collect_list',
+     'WSPM': 'collect_list',
+     'WD_VEC': 'collect_list',
+     'features': 'collect_list',
+     'aqi': 'min'})\
+    .withColumnRenamed("min(aqi)", "aqi")\
+    .withColumnRenamed("collect_list(PM2_5)", "PM2_5")\
+    .withColumnRenamed("collect_list(PM10)", "PM10")\
+    .withColumnRenamed("collect_list(SO2)", "SO2")\
+    .withColumnRenamed("collect_list(NO2)", "NO2")\
+    .withColumnRenamed("collect_list(CO)", "CO")\
+    .withColumnRenamed("collect_list(O3)", "O3")\
+    .withColumnRenamed("collect_list(TEMP)", "TEMP")\
+    .withColumnRenamed("collect_list(PRES)", "PRES")\
+    .withColumnRenamed("collect_list(DEWP)", "DEWP")\
+    .withColumnRenamed("collect_list(RAIN)", "RAIN")\
+    .withColumnRenamed("collect_list(WSPM)", "WSPM")\
+    .withColumnRenamed("collect_list(WD_VEC)", "WD_VEC")\
+    .withColumnRenamed("collect_list(features)", "features")
+
+# This should be incorporated into the pipelineModel.
+
+dataset.write.parquet('output/beijing_features_aqi_target.parquet')
+
